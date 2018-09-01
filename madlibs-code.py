74455aab42f24d15663b31ba420400abe608ef63
Sayema Islam <sayema.islam@gmail.com>
2018-09-01 12:18:01 -0700

    created function to display full madlibs story

---
+++ madlibs-code.py
@@ -0,0 +1,17 @@
+blank = "___"
+
+def display_story():
+    print(str("On my first day in " + blank +  ", of course the first thing I"))
+    print(str("wanted to " + blank +  " was " + blank + ". But how? I know I packed"))
+    print(str("enough " + blank + " to make the trek, but I wasn't not sure I trusted"))
+    print(str("my " + blank + "to allow me to do so without getting " + blank + "."))
+    print(str(""))
+    print(str("So instead, I settled on" + blank + " " + blank + ", something I could"))
+    print(str("do closer to home. Maybe I'm not an exemplary in that regard, but I"))
+    print(str("sure am" + blank + ". Plus, that was some good " + blank + "!!!"))
+
+def test():
+    print(blank)
+
+test()
+display_story()
